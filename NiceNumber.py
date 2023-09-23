e59a9abf32c8b234e20205a57f68f147fd2a68ed
RhysyT <61121117+RhysyT@users.noreply.github.com>
2023-09-23 14:53:33 +0200

    Update NiceNumber.py
    Updated so that small numbers (< 0.1) are displayed in scientific notation as well as large numbers.

--- NiceNumber.py
+++ NiceNumber.py
@@ -1,7 +1,10 @@
 # Returns numbers in a sensible format
 def nicenumber(number):
+	# Very small numbers - use scientific notation
+	if number < 0.1:
+		newnumber = str("{:.2E}".format(number))
 	# Small numbers - return comma-separated thousands rounded to 2 d.p.
-	if number < 1E6:
+	if number >= 0.01 and number < 1E6:
 		newnumber = str(f"{number:,.2f}")
 	# Large numbers - use scientific notation
 	if number >= 1E6:
