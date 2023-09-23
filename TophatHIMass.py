d45c30561a119a766a4f222c7296707cc559ea08
RhysyT <61121117+RhysyT@users.noreply.github.com>
2023-09-23 12:32:21 +0200

    Update TophatHIMass.py
    Substantial revisions : added tooltips, more explanations, equations to describe integrated S/N, numbers in string formats, and consistent solar symbol in headings and main text.

--- TophatHIMass.py
+++ TophatHIMass.py
@@ -33,13 +33,16 @@ st.write('<style>div.block-container{padding-bottom:0rem;}</style>', unsafe_allo
 
 # MAIN CODE
 st.write('#### Calculate the HI mass for a top-hat profile, specifying line width, rms, S/N and distance')
+st.write('A simple calculator to explore how the HI mass of a source varies according to its observational parameter. Assumes a top-hat profile : that is, the signal-to-noise (S/N) value is constant across the line, with the line boundaries being perfectly vertical so that W50 and W20 are identical.')
+st.write('Optionally, can also explore the corresponding integrated S/N of the same source.')
+st.write('##### Source parameters') 
 
 # A new set of columns
 left_column3, right_column3 = st.columns(2)
 
 with left_column3:
 	# Line width widget, row 1
-	linewidth = st.number_input("HI line width", key="lw")
+	linewidth = st.number_input("HI line width", key="lw", help='Approximate line width. For a top-hat profile W50 and W20 are identical')
 	
 	# Rms widget, row 2
 	rms = st.number_input("Spectra rms", key="noise")
@@ -72,7 +75,7 @@ with right_column3:
 
 
 # S/N widget, dimensionless units. Keep outside the columns.
-sn = st.number_input("Signal to noise ratio", key="snr")
+sn = st.number_input("Signal to noise ratio", key="snr", help='S/N of the source, i.e. peak flux / rms. For a top-hat profile the flux is the same everywhere across the spectral profile')
 
 
 # Optionally the user can provide other parameters for calculating the integrated S/N
@@ -81,7 +84,15 @@ totsn = None
 topflux = sn*linewidth*rms
 
 if st.checkbox('Calculate integrated S/N'):
-	st.write('Optionally provide velocity resolution, rms and line width, for calculating the integrated S/N ratio according to the ALFALFA criteria of Saintonge et al. 2007)')
+	st.write('Optionally provide velocity resolution, rms and line width, for calculating the integrated S/N ratio according to the ALFALFA criteria of Saintonge et al. 2007 :')
+	st.latex(r'''S/N =\frac{1000F_{total}}{W_{50}}\frac{w^{1/2}_{smo}}{rms}''')
+	st.write('Where w<sub style="font-size:60%">smo</sub> is a smoothing function depending on W50. If W50 &#8804; 400 km/s :', unsafe_allow_html=True)
+	st.latex(r'''w_{smo} = W50 / (2 \times v_{res})''')
+	st.write('And if W50 > 400 km/s :')
+	st.latex(r'''w_{smo} = 400 / (2 \times v_{res})''')
+	st.write('')
+	st.write('##### Observational parameters')
+		
 	dosncalc = True
 	
 	# Need to create new columns here to force a break. If we don't do this, the new parameters will be created ABOVE the checkbox, which just looks weird and 
@@ -90,7 +101,7 @@ if st.checkbox('Calculate integrated S/N'):
 	
 	with left_column2:
 		# Velocity resolution number, row 1
-		vres = st.number_input("Velocity resolution", key="vr")
+		vres = st.number_input("Velocity resolution", key="vr", help='Velocity resolution after smoothing')
 		
 	
 	with right_column2:
@@ -103,13 +114,13 @@ if st.checkbox('Calculate integrated S/N'):
 		totsn = aasn(topflux, linewidth, vres, rms)	
 	
 
-
-st.write('##### Total flux = ',topflux,' in Jy km/s')
+st.write('')
+st.write('##### Total flux = '+str(topflux)+'&thinsp;Jy&thinsp;km/s')
 
 topmass = 2.36E5 * disttop*disttop * topflux
 tophimasskg = topmass*1.98847E30
-st.write("#### Total HI mass = ", nicenumber(topmass),'M<sub style="font-size:60%">&#8857;</sub>, or ', nicenumber(tophimasskg),'kg.', unsafe_allow_html=True) 
-st.write('Exact values are',topmass,'M<sub style="font-size:60%">&#8857;</sub> and',tophimasskg,'kg.', unsafe_allow_html=True)
+st.write("#### Total HI mass = ", nicenumber(topmass),'M<sub style="font-size:60%">&#9737;</sub>, or ', nicenumber(tophimasskg),'kg.', unsafe_allow_html=True) 
+st.write('Exact values are '+str(topmass)+'&thinsp;M<sub style="font-size:60%">&#8857;</sub> and '+str(tophimasskg)+'&thinsp;kg.', unsafe_allow_html=True)
 	
 if totsn is not None:
 	st.write('#### Integrated S/N = ', nicenumber(totsn))
