7c0995799fc31f36a92e8121f94acecca5777a35
RhysyT <61121117+RhysyT@users.noreply.github.com>
2023-09-24 11:33:50 +0200

    Update NiceNumber.py
    Now evaluates the absolute value of the number, so will give the correct output format for small and large negative numbers as well as positive ones.

--- NiceNumber.py
+++ NiceNumber.py
@@ -1,13 +1,13 @@
 # Returns numbers in a sensible format
 def nicenumber(number):
 	# Very small numbers - use scientific notation
-	if number < 0.1:
+	if abs(number) < 0.1:
 		newnumber = str("{:.2E}".format(number))
 	# Small numbers - return comma-separated thousands rounded to 2 d.p.
-	if number >= 0.01 and number < 1E6:
+	if abs(number) >= 0.01 and abs(number) < 1E6:
 		newnumber = str(f"{number:,.2f}")
 	# Large numbers - use scientific notation
-	if number >= 1E6:
+	if abs(number) >= 1E6:
 		newnumber = str("{:.2E}".format(number))
 		
 	return newnumber
