6be8d727e6399e92b59e3ef60271b0a59370d5bf
RhysyT <61121117+RhysyT@users.noreply.github.com>
2024-07-12 09:42:53 +0200

    Update ICanSeeMySourceFromHere.py
    Import testing

--- ICanSeeMySourceFromHere.py
+++ ICanSeeMySourceFromHere.py
@@ -1,6 +1,6 @@
 # Program to calculate some simple parameters about source visibility, for observation planning
 
-import numpy==1.24.3
+import numpy
 import astropy
 #from astropy.coordinates import SkyCoord
 #from astropy.coordinates import AltAz 
