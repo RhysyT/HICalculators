63c6cdaa3bb450a6cf8b7fca7e9e124580809837
RhysyT <61121117+RhysyT@users.noreply.github.com>
2024-07-16 14:47:34 +0200

    Update PhotCalc.py
    Substantial revisions. Now uses astroquery to obtain the Galactic extinction at the specified coordinates, applying the correction when calculating the stellar mass. Also prints out reference information at the end.

--- PhotCalc.py
+++ PhotCalc.py
@@ -3,6 +3,11 @@ import imp
 import math as maths
 import astroquery
 
+from astroquery.irsa_dust import IrsaDust
+from astropy.coordinates import SkyCoord
+from astropy import units as u
+from astropy.coordinates import Angle
+
 # EXTERNAL SCRIPTS IMPORTED AS FUNCTIONS
 # "nicenumber" function returns human-readable versions of numbers, e.g, comma-separated or scientific notation depending
 # on size
@@ -215,8 +220,8 @@ don't use those !). Alternatively the apparent magnitudes can be given directly.
 st.markdown("""
 Providing a distance will enable calculation of absolute magnitudes. Stellar mass can then be calculated, which will 
 have improved accuracy if the user also provides sky coordinates (to account for Galactic extinction) and inclination 
-angle (to estimate internal extinction). The mass calculation requires data for the _g_ and _i_ bands; other bands are
-ignored.
+angle (to estimate internal extinction). Stellar mass is calculated using various recipes depending on which bands
+have input data, though internal extinction is only calculated for the _g_ and _i_ bands.
 """) 
 st.markdown("""More details of the methodology are given below. A crash course on aperture photometry can be found [here](https://astrorhysy.blogspot.com/2017/11/the-dark-side-of-galaxy-evolution-ii.html).""") 
 
@@ -297,7 +302,7 @@ col18, col19, col20, col21 = st.columns(4)
 sky_coord_ra = col18.text_input("RA [J2000]", help='Right Ascension, used for correcting for Galactic extinction. Enter in the format HH:MM:SS.SS or DD.DDDDDD, i.e. sexigesimal or decimal degrees.')
 sky_coord_dec = col19.text_input("Dec [J2000]", help='Reclination, used for correcting for Galactic extinction. Enter in the format DD:MM:SS.SS or DD.DDDDDD, i.e. sexigesimal or decimal degrees.')
 distance = col20.number_input("Distance (Mpc)", format="%f", min_value=0.0)
-inclination_angle = col21.number_input("Inclination Angle", format="%f", help='Inclination angle in degrees, used for internal extinction calculation. Enter a negative number to disable. By convention, 0 = face-on, 90 = edge-on. NOTE : This should assume a thin circular disc, i.e. such that cos(i) = b/a.')
+inclination_angle = col21.number_input("Inclination Angle", min_value=0.0, format="%f", help='Inclination angle in degrees, used for internal extinction calculation. Enter zero degrees to disable. By convention, 0 = face-on, 90 = edge-on. NOTE : This should assume a thin circular disc, i.e. such that cos(i) = b/a.')
 
 # Galactic Extinction Model Selection
 st.markdown("##### Galactic Extinction Model")
@@ -306,8 +311,8 @@ col16, col17 = st.columns([1, 1])
 col16.markdown("<div style='padding-top: 30px;'>Choose which Galactic reddening model to use", unsafe_allow_html=True)
 galactic_ext_model = col17.selectbox("Model", ["Schlafly & Finkbeiner 2011", "Schlegel, Finkbeiner & Davis 1998"])
 
-# Absolute Magnitudes Input
-st.markdown("##### Absolute Magnitudes")
+# Absolute Magnitudes Input (raw, uncorrected for anything except distance)
+st.markdown("##### Raw Absolute Magnitudes")
 col11, col12, col13, col14, col15 = st.columns(5)
 st.session_state['abs_mag_u'] = col11.number_input("u  ", format="%f", value=absmag(st.session_state['app_mag_u'], distance))
 st.session_state['abs_mag_g'] = col12.number_input("g  ", format="%f", value=absmag(st.session_state['app_mag_g'], distance))
@@ -316,31 +321,139 @@ st.session_state['abs_mag_i'] = col14.number_input("i  ", format="%f", value=abs
 st.session_state['abs_mag_z'] = col15.number_input("z  ", format="%f", value=absmag(st.session_state['app_mag_z'], distance))
 
 
-# *** PLACEHOLDER - HERE CORRECT FOR GALACTIC EXTINCTION ***
-# Will have to correct the parameters below to use these corrected values !
+# Corrections for Galactic extinction
+# First evaluate the input coordinates and convert them if possible. Set some placeholder
+# parameters to hold the values we will use for doing the queries. Set these to "Fail" if
+# the coordinates cannot be resolved, otherwise use the numerical values (always in decimal
+# degrees)
+queryra  = None
+querydec = None
 
-# Correct for internal extinction - ONLY APPLIED FOR G AND I BANDS !
-u_mag_int_correct = st.session_state['abs_mag_u']
-if st.session_state['abs_mag_g'] != 0.0 and inclination_angle >= 0.0:
-	g_mag_int_correct = mag_int_correct(st.session_state['abs_mag_g'], 'g', inclination_angle)
-else:
-	g_mag_int_correct = st.session_state['abs_mag_g']
-r_mag_int_correct = st.session_state['abs_mag_r']	
-if st.session_state['abs_mag_i'] != 0.0 and inclination_angle >= 0.0:
-	i_mag_int_correct = mag_int_correct(st.session_state['abs_mag_i'], 'i', inclination_angle)
-else:
-	i_mag_int_correct = st.session_state['abs_mag_i']
-z_mag_int_correct = st.session_state['abs_mag_z']
+# Also set values for correcting the extinction, from both Schlafly & Finkbeiner 2011 and
+# Schlegel, Finkbeiner & Davis 1998
+exu_SF, exu_SFD = 0.0, 0.0
+exg_SF, exg_SFD = 0.0, 0.0
+exr_SF, exr_SFD = 0.0, 0.0
+exi_SF, exi_SFD = 0.0, 0.0
+exz_SF, exz_SFD = 0.0, 0.0
 
-# Corrected Absolute Magnitudes
-st.markdown("##### Corrected Absolute Magnitudes")
-col22, col23, col24, col25, col26 = st.columns(5)
-st.session_state['corr_abs_mag_u'] = col22.number_input("u   ", format="%f", value=u_mag_int_correct)
-st.session_state['corr_abs_mag_g'] = col23.number_input("g   ", format="%f", value=g_mag_int_correct)
-st.session_state['corr_abs_mag_r'] = col24.number_input("r   ", format="%f", value=r_mag_int_correct)
-st.session_state['corr_abs_mag_i'] = col25.number_input("i   ", format="%f", value=i_mag_int_correct)
-st.session_state['corr_abs_mag_z'] = col26.number_input("z   ", format="%f", value=z_mag_int_correct)
+# Evaluate the coordinates
+if sky_coord_ra != '' and sky_coord_dec != '':
+	# If the user enters colons in the RA box, assume they're inputing HH:MM:SS.SS
+	if ':' in sky_coord_ra:
+		try:
+			queryra = Angle(sky_coord_ra+' hours').value*15.0
+		except:
+			queryra = 'Fail'
+		
 
+	# If they don't, assumme they've entered decimal degrees
+	else:
+		try:
+			queryra = Angle(sky_coord_ra+' degrees')
+		except:
+			queryra = 'Fail'
+
+
+	# If the user enters colons in the Dec box, assume they're inputing DD:MM:SS.SS
+	if ':' in sky_coord_dec:
+		try:
+			querydec = Angle(sky_coord_dec+' degrees')
+		except:
+			querydec = 'Fail'
+		
+
+	# If they don't, assumme they've entered decimal degrees
+	else:
+		try:
+			querydec = Angle(sky_coord_dec+' degrees')
+		except:
+			querydec = 'Fail'
+
+# If the coordinates have been successfully resolved, we can do the query			
+if queryra != None and queryra != 'Fail' and querydec != None and querydec != 'Fail':
+	# Create a SkyCoord object
+	coords = SkyCoord(queryra, querydec, frame='icrs', unit=(u.deg, u.deg))
+	
+	# Query the IRSA dust service
+	table = IrsaDust.get_extinction_table(coords)
+
+	# Get rows corresponding to SDSS bands
+	sdss_u_band_row = table[table['Filter_name'] == 'SDSS u']
+	sdss_g_band_row = table[table['Filter_name'] == 'SDSS g']
+	sdss_r_band_row = table[table['Filter_name'] == 'SDSS r']
+	sdss_i_band_row = table[table['Filter_name'] == 'SDSS i']
+	sdss_z_band_row = table[table['Filter_name'] == 'SDSS z']
+
+	# Retrieve the extinction values from the A_SandFmag (Schlafly & Finkbeiner 2011) column
+	exu_SF = sdss_u_band_row['A_SandF'][0]
+	exg_SF = sdss_g_band_row['A_SandF'][0]
+	exr_SF = sdss_r_band_row['A_SandF'][0]
+	exi_SF = sdss_i_band_row['A_SandF'][0]
+	exz_SF = sdss_z_band_row['A_SandF'][0]
+
+	# Also the A_SFD (Schlegel, Finkbeiner & Davis 1998) values
+	exu_SFD = sdss_u_band_row['A_SFD'][0]
+	exg_SFD = sdss_g_band_row['A_SFD'][0]
+	exr_SFD = sdss_r_band_row['A_SFD'][0]
+	exi_SFD = sdss_i_band_row['A_SFD'][0]
+	exz_SFD = sdss_z_band_row['A_SFD'][0]
+
+
+	# Now we apply the corrections, depending on which model the user selected. Apply the
+	# corrections to the apparent magnitudes, then convert to absolute
+	if galactic_ext_model == 'Schlafly & Finkbeiner 2011':
+		u_GalCorrected = st.session_state['app_mag_u'] - exu_SF
+		g_GalCorrected = st.session_state['app_mag_g'] - exg_SF
+		r_GalCorrected = st.session_state['app_mag_r'] - exr_SF
+		i_GalCorrected = st.session_state['app_mag_i'] - exi_SF
+		z_GalCorrected = st.session_state['app_mag_z'] - exz_SF
+	
+	if galactic_ext_model =='Schlegel, Finkbeiner & Davis 1998':
+		u_GalCorrected = st.session_state['app_mag_u'] - exu_SFD
+		g_GalCorrected = st.session_state['app_mag_g'] - exg_SFD
+		r_GalCorrected = st.session_state['app_mag_r'] - exr_SFD
+		i_GalCorrected = st.session_state['app_mag_i'] - exi_SFD
+		z_GalCorrected = st.session_state['app_mag_z'] - exz_SFD
+	
+	# Now convert to absolute magnitudes
+	abs_u_GalCorrected  = absmag(u_GalCorrected, distance)
+	abs_g_GalCorrected  = absmag(g_GalCorrected, distance)
+	abs_r_GalCorrected  = absmag(r_GalCorrected, distance)
+	abs_i_GalCorrected  = absmag(i_GalCorrected, distance)
+	abs_z_GalCorrected  = absmag(z_GalCorrected, distance)	
+
+
+	# Correct for internal extinction - ONLY APPLIED FOR G AND I BANDS ! u,r,z all use only the Galactic
+	# extinction correction
+	u_mag_int_correct = abs_u_GalCorrected
+	if st.session_state['abs_mag_g'] != 0.0 and inclination_angle >= 0.0:
+		g_mag_int_correct = mag_int_correct(abs_g_GalCorrected, 'g', inclination_angle)
+	else:
+		g_mag_int_correct = abs_g_GalCorrected
+	r_mag_int_correct = abs_r_GalCorrected
+	if st.session_state['abs_mag_i'] != 0.0 and inclination_angle >= 0.0:
+		i_mag_int_correct = mag_int_correct(abs_i_GalCorrected, 'i', inclination_angle)
+	else:
+		i_mag_int_correct = abs_i_GalCorrected
+	z_mag_int_correct = abs_z_GalCorrected
+
+
+	# Corrected Absolute Magnitudes
+	st.markdown("##### Extinction-Corrected Absolute Magnitudes")
+	st.write('These values are corrected for both internal and Galactic extinction.')
+	col22, col23, col24, col25, col26 = st.columns(5)
+	# Only do the correction if the uncorrected value was not zero
+	if st.session_state['abs_mag_u'] != 0.0:
+		st.session_state['corr_abs_mag_u'] = col22.number_input("u   ", format="%f", value=u_mag_int_correct)
+	if st.session_state['abs_mag_g'] != 0.0:
+		st.session_state['corr_abs_mag_g'] = col23.number_input("g   ", format="%f", value=g_mag_int_correct)
+	if st.session_state['abs_mag_r'] != 0.0:
+		st.session_state['corr_abs_mag_r'] = col24.number_input("r   ", format="%f", value=r_mag_int_correct)
+	if st.session_state['abs_mag_i'] != 0.0:
+		st.session_state['corr_abs_mag_i'] = col25.number_input("i   ", format="%f", value=i_mag_int_correct)
+	if st.session_state['abs_mag_z'] != 0.0:
+		st.session_state['corr_abs_mag_z'] = col26.number_input("z   ", format="%f", value=z_mag_int_correct)
 
 
 # We can now calculate the stellar mass ! Use many different prescriptions. Check which wavebands are available for a viable
@@ -358,7 +471,6 @@ goodr = True if goodr < 0.0 else False
 goodi = True if goodi < 0.0 else False
 goodz = True if goodz < 0.0 else False
 
-print(st.session_state['corr_abs_mag_g'], goodg)
 
 # Work out which stellar masses we can compute : optical colours, UV/optical, UV/NIR, optical/NIR
 opticalstellarmass = False
@@ -448,3 +560,11 @@ if opnifrdstellarmass == True:
 	
 	st.write('##### Stellar mass (r-z) Bell+2003   = ', nicenumber(galaxy_stellar_mass),'&thinsp;M<sub style="font-size:80%">&#9737;</sub>', unsafe_allow_html=True)
 	st.write('(r-z) =',nicenumber(st.session_state['corr_abs_mag_r'] - st.session_state['corr_abs_mag_z']))
+
+
+# Print additional information if any stellar mass calculation was done
+if True in [opticalstellarmass, uvoptclstellarmass, uvnifrdstellarmass, opnifrdstellarmass]:
+	st.write('### Reference Information')
+	st.write('The stellar mass calculations here follow the methods outlined in [Taylor et al. 2022](https://ui.adsabs.harvard.edu/abs/2022AJ....164..233T/abstract) and [Durbala et al. 2020](https://ui.adsabs.harvard.edu/abs/2020AJ....160..271D/abstract). The (g-i) method comes from [Taylor et al. 2011](https://ui.adsabs.harvard.edu/abs/2011MNRAS.418.1587T/abstract) while all the others are taken from [Bell et al. 2003](https://ui.adsabs.harvard.edu/abs/2003ApJS..149..289B/abstract) (table 7). The absolute magnitude of the Sun in the different bands was taken from [here](https://mips.as.arizona.edu/~cnaw/sun_2006.html).')
+	st.write('These methods are designed for normal, star-forming galaxies. They may break down for extreme objects.')
+	st.write('The Galactic extinction corrections use the models from [Schlegel, Finkbeiner & Davis 1998](https://ui.adsabs.harvard.edu/abs/1998ApJ...500..525S/abstract) and [Schlafly & Finkbeiner 2011](https://ui.adsabs.harvard.edu/abs/2011ApJ...737..103S/abstract), the standard corrections used in [NED](https://ned.ipac.caltech.edu/classic).')
